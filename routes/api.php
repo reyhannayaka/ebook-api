7adbe207498a60b65c4faf825a52e3b7a3f04a41
Reyhan <310120192@student.smktelkom-pwt.sch.id>
2022-10-24 11:31:45 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('me',[AuthController::class, 'me']);
    
+Route::resource('books', BookController::class)->except(
+    ['create','edit']
+);
