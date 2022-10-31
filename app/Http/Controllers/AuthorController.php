a0a85776289882a713623ea9b78f763c18f0c55e
Reyhan <310120192@student.smktelkom-pwt.sch.id>
2022-10-31 10:36:14 +0700

    push challenge author

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author; 
+
+use function PHPUnit\Framework\returnSelf;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $author = author::all();
+        return $author;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new author();
+        $data->name = $request->input('name');
+        $data->date_of_birth = $request->input('date_of_birth');
+        $data->place_of_birth = $request->input('place_of_birth');
+        $data->gender = $request->input('gender');
+        $data->email = $request->input('email');
+        $data->hp = $request->input('hp');
+        $data->save();
+
+        return response()->json([
+            'status' => 201, 
+            'data' => $data
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = author::find($id); 
+        if ($data) {
+            return response()->json([
+                'status' => 200, 
+                'message' => "Success get data",
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message'=> 'id ' . $id . ' tidak ditemukan'
+            ], 404);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = new author();
+        $data = author::find($id);
+        $data->name = $request->name;
+        $data->date_of_birth = $request->date_of_birth;
+        $data->place_of_birth = $request->place_of_birth;
+        $data->gender = $request->gender;
+        $data->email = $request->email;
+        $data->hp = $request->hp;
+        $data->save();
+
+        return response()->json([
+            'status' => 201, 
+            'data' => $data
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = author::where('id', $id)->first();
+        if($data){
+            $data->delete();
+            return response()->json([
+                'status' => 200, 
+                'message'=> 'id ' . $id . ' berhasil di hapus',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'id' . $id . ' tidak ditemukan'
+            ], 404);
+        }
+    }
+}
